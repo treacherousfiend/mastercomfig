e2de2361398cf9ffbccfd0937ecd77c46b515598
mastercoms <mastercoms@tuta.io>
2024-05-07 00:21:35 -0400

    more opengl removal

--- dev/common.sh
+++ dev/common.sh
@@ -8,13 +8,13 @@ function cleanItems {
     # remove blank lines
     find . \( -name "*.cfg" -o -name "*.txt" -o -name "*.res" \) -print0 | xargs -0 sed -i '/^\s*$/d'
     # remove quotes from VDF key values TODO: don't remove empty quotes or spaced strings
-    find . \( -name "mtp.cfg" -o -name "dxsupport*.cfg" -o -name "glbaseshaders*.cfg" \
+    find . \( -name "mtp.cfg" -o -name "dxsupport*.cfg"" \
      -o -name "*.txt" -and ! -name "texture_preload_list.txt" -o -name "*.res" \) -print0 | xargs -0 -I{} ../shrink_key_values.sh {}
     # Remove newlines from VDF key values
-    find . \( -name "mtp.cfg" -o -name "dxsupport*.cfg" -o -name "glbaseshaders*.cfg" \
+    find . \( -name "mtp.cfg" -o -name "dxsupport*.cfg"" \
      -o -name "*.txt" -and ! -name "texture_preload_list.txt" -o -name "*.res" \) -print0 | xargs -0 sed -i -z 's/\n/ /g'
     # remove extraneous whitespace from VDF key values
-    find . \( -name "mtp.cfg" -o -name "dxsupport*.cfg" -o -name "glbaseshaders*.cfg" \
+    find . \( -name "mtp.cfg" -o -name "dxsupport*.cfg"" \
      -o -name "*.txt" -o -name "*.res" \) -print0 | xargs -0 sed -i -e "s/[[:space:]]\+/ /g"
   fi
 }
